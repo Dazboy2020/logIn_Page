e6989dbbd1808c0a0f75a3e1ca628668df77accc
Dazboy2020 <darrendowlut@tutanota.com>
2022-12-08 20:42:37 +0100

    logIn error message added

--- script.js
+++ script.js
@@ -7,15 +7,22 @@ const account1 = {
 
 const accounts = [account1];
 
-//! Elements
+//! ELEMENTS
 const inputLoginUsername = document.querySelector(".username__input");
 const inputLoginPin = document.querySelector(".password__input");
+
+//!DISPLAY WINDOWS SIGN_UP & CREATE ACCOUNT
 const displaySignUpWindow = document.querySelector(".container2");
 const displayLogInWindow = document.querySelector(".container");
 
-//? Buttons
+//! ERROR MESSAGES
+const login__details__incorrect = document.querySelector(".error__message");
+
+//!BUTTONS
 const btnLogin = document.querySelector(".submit__btn");
 const btnSign = document.querySelector(".sign__btn");
+
+//!!  ########### FUNCTIONS #############
 
 //! LOG_IN function
 btnLogin.addEventListener("click", function (e) {
@@ -28,9 +35,25 @@ btnLogin.addEventListener("click", function (e) {
 	if (currentAccount?.pin === +inputLoginPin.value) {
 		console.log("success");
 	} else {
-		console.log("fail");
+		clearLoginInputs();
+		displayIncorrectLoginError();
 	}
 });
+
+//!Clear Login input fields function
+
+const clearLoginInputs = function () {
+	inputLoginUsername.value = "";
+	inputLoginPin.value = "";
+};
+
+//! Display incorrect error message
+const displayIncorrectLoginError = function () {
+	login__details__incorrect.classList.remove("hidden");
+	setTimeout(() => {
+		login__details__incorrect.classList.add("hidden");
+	}, "3000");
+};
 
 //! SIGN_UP Button Event Handler
 btnSign.addEventListener("click", function (e) {
