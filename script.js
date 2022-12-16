425f3cc79292cba0e083f7457c88510d7ab52f9e
Dazboy2020 <darrendowlut@tutanota.com>
2022-12-17 00:16:25 +0100

    added date and year to clock

--- script.js
+++ script.js
@@ -161,6 +161,8 @@ const showTime = () => {
 		const now = new Date();
 		const options = {
 			weekday: "long",
+			day: "2-digit",
+			year: "numeric",
 			hour: "2-digit",
 			minute: "2-digit",
 			second: "2-digit",
